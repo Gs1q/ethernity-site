679ff6b74c96fe85bcd290d97f5c9b11bcc6ab01
Gs1q <139647534+Gs1q@users.noreply.github.com>
2025-07-17 10:25:26 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 document.getElementById("discord-login").addEventListener("click", () => {
-  const clientId = "1080748181752651776";
+  const clientId = "https://discord.com/oauth2/authorize?client_id=1395303543832969337&response_type=code&redirect_uri=https%3A%2F%2Fethernity-site.vercel.app%2F&scope=identify";
   const redirectUri = encodeURIComponent("https://ethernity.vercel.app/");
   const discordUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=identify`;
 
   window.location.href = discordUrl;
+});
+// Обработка токена и отображение инфы
+window.addEventListener("load", () => {
+  const hash = window.location.hash;
+  if (hash.includes("access_token")) {
+    const token = new URLSearchParams(hash.substring(1)).get("access_token");
+
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(user => {
+        // 👇 Выводим инфу о пользователе (можно стилизовать как хочешь)
+        const profileDiv = document.createElement("div");
+        profileDiv.innerHTML = `
+          <p style="margin-top: 30px;">Вы вошли как <b>${user.username}#${user.discriminator}</b></p>
+          <img src="https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png" width="100" style="border-radius: 50%;" />
+        `;
+        document.body.appendChild(profileDiv);
+      });
+  }
 });
